6807d1858936dc30250d2a653fd80420375419f3
adinanhamza <adinanck4557@gmail.com>
2025-04-08 18:46:15 +0530

    added service

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,14 +1,27 @@
 class studentModel{
-  String id;
+  String? id;
   String name;
   String studentClass;
   String address;
 
-  studentModel({required this.address,required this.id,required this.name,required this.studentClass});
+  studentModel({required this.address, this.id,required this.name,required this.studentClass});
 
   Map<String,dynamic>toDataBase(){
     return {
-
+'id' : id,
+'name': name,
+'studentClass' : studentClass,
+'address' : address,
     };
+  }
+
+
+  factory studentModel.fromDataBase(Map<String,dynamic>json,String id){
+    return studentModel(
+      id: json['id'].toString(),
+      name: json['name'] ?? '',
+      address: json['address'] ?? '',
+      studentClass: json['studentClass'] ?? ''
+          );
   }
 }
--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,3 +1,48 @@
-class studentService{
-   
+import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_sample/model/model.dart';
+
+class StudentService {
+  final CollectionReference fireBaseData = FirebaseFirestore.instance
+      .collection('data');
+
+  Future<void> addStudent(studentModel studentdata) async {
+    try {
+      log('add data : ${studentdata.name}');
+      await fireBaseData.add(studentdata.toDataBase());
+    } catch (e) {
+      log('failed to add student data  : $e');
+    }
+  }
+
+  Future<List<studentModel>> getStudentData()async{
+    try {
+      final QuerySnapshot snapshot = await fireBaseData.get();
+      return snapshot.docs.map((doc)=> studentModel.fromDataBase(doc.data() as Map<String,dynamic>, doc.id)).toList();
+    } catch (e) {
+      log('failed to fetch student data  : $e' );
+      throw Exception('error fetching data');
+    }
+  }
+
+  Future<void> updateStudent(studentModel updatedStudent, String id) async {
+    try {
+      await fireBaseData.doc(id).update(updatedStudent.toDataBase());
+      log('update student data : ${updatedStudent.id}');
+    } catch (e) {
+      log('failed to update student : $e');
+    }
+  }
+
+  Future<void> deleteStudent(String id) async {
+    try {
+      await fireBaseData.doc(id).delete();
+      log('successfully deleted student id :$id ');
+    } on FirebaseException catch (e) {
+      log('failed to delete student : $e');
+    }
+  }
+
+
 }
